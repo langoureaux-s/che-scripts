48537e1f6824d0bcdad0d063869b6a24114a8700
disaster37 <linuxworkgroup@hotmail.com>
2019-11-18 10:24:05 +0100

    Create alpine.sh

---
+++ alpine.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+GID=1724
+UID=1724
+GROUP=dev
+USER=dev
+APP_HOME=/home/dev
+
+# Install extra package
+apk --update add sudo git wget curl ca-certificates iputils zip vim make bash &&\
+rm -rf /var/cache/apk/*
+
+# Create user and group
+addgroup -g ${GID} ${GROUP} && \
+adduser -g "${USER} user" -D -h ${APP_HOME} -G ${GROUP} -s /bin/bash -u ${UID} ${USER} &&\
+echo "%dev ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/dev
+
+# Install gitflow
+cd /opt
+curl -LO https://raw.github.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh
+sh gitflow-installer.sh install stable
+rm gitflow-installer.sh
